558e729f43cb87da6e6e50fe373c6c2ade3541ab
Smeppitty <dmsolinsky@email.wm.edu>
2021-04-18 05:05:46 -0400

    added month button hehe

--- src/script.js
+++ src/script.js
@@ -1,23 +1,22 @@
 var clicks = 0
 
-function counter() {
+function counter(daysInYear) {
 
 	const counters = document.querySelectorAll('.counter');
 	const speed = 200; // The lower the slower
-  clicks++;
-  
+  	clicks++;  
 
 	counters.forEach(counter => {
 		const updateCount = () => {
 			const target = +Math.ceil(counter.getAttribute('max'));
-			const subtarget = +Math.ceil(counter.getAttribute('max')/365);
+			const subtarget = +Math.ceil(counter.getAttribute('max')/daysInYear);
 			const count = +Math.ceil(counter.innerText);
 
 			// Lower inc to slow and higher to slow
 			const inc = Math.ceil(subtarget / speed);
 
       if (count >= target) {
-				counter.innerText = target;
+			counter.innerText = target;
 			//if total target was not reached, and if current value is not below daily value, then set current value to the daily value
 			}else if (count < subtarget*clicks) {
 				// Add inc to count and output in counter
