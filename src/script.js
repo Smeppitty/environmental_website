111f34e789da897f8b0734488ee9deb62cf5a0f7
Anna <amj0851@gmail.com>
2021-04-18 02:42:07 -0400

    sigh

--- src/script.js
+++ src/script.js
@@ -1,18 +1,3 @@
-var width = 0;
+const counters = document.querySelectorAll('.counter');
 
-function move(ID) {
-  var elem = document.getElementById(ID);
-    if (width < 100) {
-      if(ID == "Animalbar"){
-        width+=277
-        elem.style.width = width + '%';
-        elem.innerHTML = width * 1 + 'Dead';
-      }else if(ID == "Airbar"){
-        //do other things
-      }
-    }
-    if(width == 100){
-      //pop up
-    }
-  
-}
+
